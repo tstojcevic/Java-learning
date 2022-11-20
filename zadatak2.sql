2d1ddc3200f04ba6da7d06d50e013f205c88854f
Toni Stojčević <stojcevict@gmail.com>
2022-11-20 18:22:02 +0100

    Create zadatak2.sql

---
+++ zadatak2.sql
@@ -0,0 +1,19 @@
+# C:\Users\Korisnik\Documents\Java-learning\zadatak2.sql
+
+drop database if exists samouprava;
+create database samouprava;
+use samouprava;
+
+create table zupanija (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    zupan varchar(50)
+);
+
+create table opcina (
+    sifra int not null primary key auto_increment,
+    zupanija int,
+    naziv varchar (50)
+);
+
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
