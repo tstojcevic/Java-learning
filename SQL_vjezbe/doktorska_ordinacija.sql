096793690071bc274f54a5319b352c2b9bf9f1da
Toni Stojčević <stojcevict@gmail.com>
2022-11-27 18:10:07 +0100

    Create doktorska_ordinacija.sql

---
+++ SQL_vjezbe/doktorska_ordinacija.sql
@@ -0,0 +1,33 @@
+# xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < D:\Java-learning\SQL_vjezbe\doktorska_ordinacija.sql
+
+drop database if exists doktorska_odrdinacija;
+create database doktorska_odrdinacija;
+use doktorska_odrdinacija;
+
+create table doktor (
+    sifra int not null primary key auto_increment,
+    ime varchar(20),
+    prezime varchar (20),
+    specijalizacija varchar(50)
+);
+
+create table pacijent (
+    sifra int not null primary key auto_increment,
+    ime varchar (20),
+    prezime varchar (20),
+    bolest varchar (50)
+);
+
+create table medicinska_sestra (
+    sifra int not null primary key auto_increment,
+    ime varchar (20),
+    prezime varchar (20),
+    rang varchar (20),
+    doktor int
+);
+
+create table lijecenje (
+    doktor int,
+    pacijent int
+);
+
