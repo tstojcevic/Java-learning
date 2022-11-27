8d7970ad98d5a9dfb76e0534e499c8921ec71898
Toni Stojčević <stojcevict@gmail.com>
2022-11-27 21:03:27 +0100

    unos dodatne tablice i ispravak vanjskog ključa

--- SQL_vjezbe/postolar.sql
+++ SQL_vjezbe/postolar.sql
@@ -12,6 +12,13 @@ create table obuca (
     korisnik int
 );
 
+create table korisnik (
+    sifra int not null primary key auto_increment,
+    ime varchar (20),
+    prezime varchar(20),
+    broj_mobitela varchar (15)
+);
+
 create table popravak (
     sifra int not null primary key auto_increment,
     cijena decimal (18,2),
@@ -31,8 +38,8 @@ create table obuca_popravak (
     popravak int
 );
 
-alter table obuca add foreign key (korisnik) references obuca (sifra);
 alter table popravak add foreign key (segrt) references segrt (sifra);
 alter table obuca_popravak add foreign key (obuca) references obuca (sifra);
 alter table obuca_popravak add foreign key (popravak) references popravak (sifra);
+alter table obuca add foreign key (korisnik) references korisnik (sifra);
 
